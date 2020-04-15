2b8e6911e50655ad6503132335e5fb2d38d81bd6
ayu <2531620933@qq.com>
2020-04-15 14:48:38 +0800

    first commit

---
+++ gmall-admin-web/src/main/java/com/guozeyu/gmall/admin/GmallAdminApplication.java
@@ -0,0 +1,13 @@
+package com.guozeyu.gmall.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GmallAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GmallAdminApplication.class,args);
+    }
+
+}
---
+++ gmall-pms/src/test/java/com/guozeyu/gmall/gmall/pms/GmallPmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guozeyu.gmall.gmall.pms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GmallPmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
